9845945ee05b3b0e53883d21e0d87404c8f7cbe3
alex-marrero <alex.marrero215@outlook.com>
2020-09-28 15:20:09 -0400

    Add Packer json and software install script

---
+++ software_installs.ps1
@@ -0,0 +1,8 @@
+ECHO Installing apps
+
+ECHO Configure chocolatey
+choco feature enable -n allowGlobalConfirmation
+choco install notepadplusplus
+choco install 7zip
+choco install sysinternals
+choco install firefox
