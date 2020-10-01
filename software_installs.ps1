99e1f45a292df7883587814e78eed1cfbfbd807d
Alexander Marrero <58573200+alex-marrero@users.noreply.github.com>
2020-10-01 10:38:54 -0400

    Delete software_installs.ps1
    Removed Script

--- software_installs.ps1
+++
@@ -1,8 +0,0 @@
-ECHO Installing apps
-
-ECHO Configure chocolatey
-choco feature enable -n allowGlobalConfirmation
-choco install notepadplusplus
-choco install 7zip
-choco install sysinternals
-choco install firefox
